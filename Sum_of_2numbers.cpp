ad0a5a19c4e0744fd3960769a7ba6e19cc7c2e63
Indrajith G Nair <indrajithgnair2007@gmail.com>
2025-08-12 21:03:35 +0530

    Add files via upload
    This is an code to find the sum of 2 numbers

---
+++ Sum_of_2numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1,num2,sum;//initialising 3 variables
+	cout<<"Enter the 1st number : ";//asking the user to enter the first number
+	cin>>num1;//storing it in num1
+	cout<<"Enter the 2nd number : ";//asking the user to enter second number
+	cin>>num2;//storing it in num2
+	sum=num1+num2;
+	cout<<"The sum of "<<num1<<" and "<<num2<<" is : "<<sum;//printing the sum of the values
+	return 0;
+}
